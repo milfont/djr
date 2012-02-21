ab77e99346ba35ca9359a93dae102286eabbb1ec
Christiano Milfont <cmilfont@gmail.com>
2012-02-21 11:01:48 -0300

    removendo arquivos desnecessarios

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Djr::Application.config.secret_token = 'ea5e283e2ba06cffc3643ddd948d7d1612a591444c27348dcfb8bbafb8b040a1d2421dede351f77bdefa069849ba662ca9d07e02ee4f92ae47ecf88669208606'
--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Djr::Application.config.session_store :cookie_store, key: '_djr_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Djr::Application.config.session_store :active_record_store
