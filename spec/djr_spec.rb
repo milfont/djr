c57d221ca0be637582e86b8a233ad54278e7fa9d
Christiano Milfont <cmilfont@gmail.com>
2012-02-21 12:38:50 -0300

    configurando para travis-ci

--- spec/djr_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-
-describe "Djr" do
-  it "Generate js" do
-    
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
-end
